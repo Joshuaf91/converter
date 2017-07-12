234b57a234124f2f89967ec25418341f3f040f02
Joshua Fermin <Joshuaf91@gmail.com>
2017-07-12 17:28:00 -0400

    get video info and display

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
+const _ = require('lodash');
 
-const { app , BrowserWindow } = electron;
+const { app , BrowserWindow, ipcMain } = electron;
 
 let mainWindow;
 
@@ -12,4 +14,25 @@ app.on('ready', () => {
   });
 
   mainWindow.loadURL(`file://${__dirname}/src/index.html`)
+});
+
+ipcMain.on('videos:added', (event, videos) => {
+  const promises = _.map(videos, video => {
+    return new Promise((resolve, reject) => {
+      ffmpeg.ffprobe(video.path, (err, metadata) => {
+        resolve(
+          Object.assign({},video,
+          {
+          duration: metadata.format.duration,
+          format: 'avi'
+          })
+        )
+      })
+    })
+  })
+
+  Promise.all(promises)
+    .then((results) => {
+      mainWindow.webContents.send('metadata:complete', results)
+    });
 });
